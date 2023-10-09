26d8eb33d5970649b8e2809c453bfcbc573bdec4
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2023-10-09 14:05:53 +0300

    добавила переменную needUpdate, которая отвечает за цвет кнопки обновить

--- pages/archive.vue
+++ pages/archive.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <div class="header">
-      <p class="nameStantion">Название старнции</p>
+      <p class="nameStantion">Название станции</p>
       <v-btn
         class="mx-2"
-        color="blue-grey-lighten-4"
         icon="mdi-reload"
         size="x-small"
         variant="tonal"
+        :class="[needUpdate ? 'text-red' : 'text-blue-grey-lighten-4']"
         @click="this.$router.go()"
       ></v-btn>
     </div>
   </div>
   <div class="wrapper_form">
     <DatePick v-model:date="date"></DatePick>
-    <vSearch v-model="searchText" @searchTrain="searchTrain(searchText)"></vSearch>
+    <vSearch v-model="searchText" @searchTrain="searchTrain(searchText)"></vSearch>l;
   </div>
 </template>
 <script setup>
@@ -23,9 +23,10 @@ import DatePick from "../components/date-pick.vue";
 import vSearch from "~/components/v-search.vue";
 import { useToast } from "vue-toastification";
 
-const date = ref(new Date());
-let searchText = ref(" ");
-let needUpdate;
+const date = ref(new Date()); //дата из календаря
+let searchText = ref(" "); //переменная, для текста из поисковой строки
+let needUpdate = ref(false); //нужно ли обовить страницу
+
 function searchTrain(text) {
   useToast().clear();
   if (Number(text)) {
